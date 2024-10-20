bfb22a10e3c597fd002dd61a969f4354b2366553
Evgeny Dudarev <e.dudarev@hh.ru>
2024-10-20 20:52:23 +0300

    main fixed cross origin for conttroller

--- src/main/java/com/example/dudarev/controller/RegistrationController.java
+++ src/main/java/com/example/dudarev/controller/RegistrationController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://frontend:3000")
 public class RegistrationController {
 
     @PostMapping("/register")
